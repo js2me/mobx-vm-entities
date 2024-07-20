7b2b64120b69858fc5254df4f3d74e6f0d66f3f4
Sergey Volkov <js2me@outlook.com>
2024-07-20 15:44:20 +0300

    fix: missing type exports

--- src/view-model/index.ts
+++ src/view-model/index.ts
@@ -1,4 +1,6 @@
 export * from './view-model';
 export * from './abstract-view-model.impl';
+export * from './abstract-view-model.types';
 export * from './view-model.store';
 export * from './view-model.store.impl';
+export * from './view-model.store.types';
