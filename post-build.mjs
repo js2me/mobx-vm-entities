9ec3564ba3309f246d689e4514b8a8d312056d4c
Sergey Volkov <js2me@outlook.com>
2024-11-04 12:45:30 +0300

    internal: improve publish script

--- post-build.mjs
+++ post-build.mjs
@@ -1,8 +1,23 @@
-import { postBuildScript } from 'js2me-exports-post-build-script';
+import { postBuildScript, publishScript } from 'js2me-exports-post-build-script';
 
 postBuildScript({
   buildDir: 'dist',
   rootDir: '.',
   srcDirName: 'src',
   filesToCopy: ['LICENSE', 'README.md'],
+  updateVersion: process.env.PUBLISH_VERSION,
+  onPackageVersionChanged: (nextVersion, currVersion) => {
+    if (process.env.PUBLISH) {
+      publishScript({
+        nextVersion,
+        currVersion,
+        publishCommand: 'pnpm publish',
+        commitAllCurrentChanges: true,
+        createTag: true,
+        githubRepoLink: 'https://github.com/js2me/mobx-vm-entities',
+        cleanupCommand: 'pnpm clean', 
+      })
+    }
+  }
 });
+
