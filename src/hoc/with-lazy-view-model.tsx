8066268085ca06379b210246b5cbc3041f0a4667
Sergey Volkov <js2me@outlook.com>
2024-11-24 20:16:43 +0300

    fix: try to fix invalid typings for withLazyViewModel HOC

--- src/hoc/with-lazy-view-model.tsx
+++ src/hoc/with-lazy-view-model.tsx
@@ -1,21 +1,35 @@
-import { ComponentType } from 'react';
+import { ComponentProps, ComponentType, ReactNode } from 'react';
 import { loadable } from 'react-simple-loadable';
 
 import { Class } from '../utils/types';
+import { ViewModel } from '../view-model';
 
-import { ViewModelHocConfig, withViewModel } from './with-view-model';
+import {
+  ViewModelHocConfig,
+  ViewModelInputProps,
+  withViewModel,
+} from './with-view-model';
 
-interface LazyViewAndModel {
-  Model: Class<any>;
-  View: ComponentType<any>;
+interface LazyViewAndModel<
+  TViewModel extends ViewModel<any>,
+  TView extends ComponentType<any>,
+> {
+  Model: Class<TViewModel>;
+  View: TView;
 }
 
-export function withLazyViewModel(
-  loadFunction: () => Promise<LazyViewAndModel>,
+export function withLazyViewModel<
+  TViewModel extends ViewModel<any>,
+  TView extends ComponentType<any>,
+>(
+  loadFunction: () => Promise<LazyViewAndModel<TViewModel, TView>>,
   config?: ViewModelHocConfig<any>,
 ) {
   return loadable(async () => {
     const { Model, View } = await loadFunction();
     return withViewModel(Model, config)(View);
-  }, config?.fallback);
+  }, config?.fallback) as unknown as (
+    props: Omit<ComponentProps<TView>, 'model'> &
+      ViewModelInputProps<TViewModel>,
+  ) => ReactNode;
 }
