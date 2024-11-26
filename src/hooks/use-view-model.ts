d3220efc34f5ba1d754a809cad0beee22ec15c9e
Sergey Volkov <js2me@outlook.com>
2024-11-26 17:51:08 +0300

    fix: useViewModel hook typings

--- src/hooks/use-view-model.ts
+++ src/hooks/use-view-model.ts
@@ -4,7 +4,7 @@ import { ActiveViewContext, ViewModelsContext } from '../contexts';
 import { Class, Maybe } from '../utils/types';
 import type { ViewModel } from '../view-model/view-model';
 
-export const useViewModel = <T extends ViewModel<any>>(
+export const useViewModel = <T extends ViewModel<any, any>>(
   idOrClass?: Maybe<string> | Class<T>,
 ): T => {
   const viewModels = useContext(ViewModelsContext);
