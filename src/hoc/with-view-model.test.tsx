c8a60920ab7bba769fd539d7ad08f75a7e36da81
Sergey Volkov <js2me@outlook.com>
2024-11-30 15:18:00 +0300

    test: add tests for withViewModel HOC

--- src/hoc/with-view-model.test.tsx
+++ src/hoc/with-view-model.test.tsx
@@ -1,20 +1,75 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
-import { ViewModelImpl } from '../view-model';
+import { TestViewModelImpl } from '../view-model/view-model.impl.test';
 
-import { view } from './view';
-import { withViewModel } from './with-view-model';
+import { ViewModelProps, withViewModel } from './with-view-model';
 
 describe('withViewModel', () => {
   test('renders', () => {
-    class VM extends ViewModelImpl {}
-    const View = view<VM>(({ model }) => {
+    class VM extends TestViewModelImpl {}
+    const View = ({ model }: ViewModelProps<VM>) => {
       return <div>{`hello ${model.id}`}</div>;
-    });
+    };
     const Component = withViewModel(VM)(View);
 
     render(<Component />);
     expect(screen.getByText('hello VM_0_00000')).toBeDefined();
   });
+
+  test('renders twice', async () => {
+    class VM extends TestViewModelImpl {}
+    const View = ({ model }: ViewModelProps<VM>) => {
+      return <div>{`hello ${model.id}`}</div>;
+    };
+    const Component = withViewModel(VM)(View);
+
+    render(
+      <>
+        <Component />
+        <Component />
+      </>,
+    );
+    expect(screen.getByText('hello VM_1_00000')).toBeDefined();
+    expect(screen.getByText('hello VM_1_00001')).toBeDefined();
+  });
+
+  test('renders with fixed id', () => {
+    class VM extends TestViewModelImpl {}
+    const View = ({ model }: ViewModelProps<VM>) => {
+      return <div>{`hello ${model.id}`}</div>;
+    };
+    const Component = withViewModel(VM, { id: 'my-test' })(View);
+
+    render(<Component />);
+    expect(screen.getByText('hello my-test')).toBeDefined();
+  });
+
+  test('renders twice with fixed id', async () => {
+    class VM extends TestViewModelImpl {}
+    const View = ({ model }: ViewModelProps<VM>) => {
+      return <div>{`hello ${model.id}`}</div>;
+    };
+    const Component = withViewModel(VM, { id: 'my-test' })(View);
+
+    render(
+      <>
+        <Component />
+        <Component />
+      </>,
+    );
+    expect(await screen.findAllByText('hello my-test')).toHaveLength(2);
+  });
+
+  // test('renders with view model store', () => {
+  //   class VM extends TestViewModelImpl {}
+  //   const View = ({ model }: ViewModelProps<VM>) => {
+  //     return <div>{`hello ${model.id}`}</div>;
+  //   };
+  //   const Component = withViewModel(VM)(View);
+  //   const vmStore = new TestViewModelStoreImpl();
+
+  //   render(<Component />);
+  //   expect(screen.getByText('hello VM_0_00000')).toBeDefined();
+  // });
 });
