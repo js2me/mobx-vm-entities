9e166c294d73e3bf053041d7e3b2a4c103195798
Sergey Volkov <js2me@outlook.com>
2024-12-02 12:26:07 +0300

    test: remove jest-dom (useless library)

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: './setupTests.js',
     coverage: {
       provider: 'istanbul' // or 'v8'
     },
